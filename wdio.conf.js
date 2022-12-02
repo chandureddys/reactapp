d1d119955d3d11f5789b74f5cd4d94913eb48c28
Reddy <csreddy.512@gmail.com>
2022-12-01 22:10:31 -0600

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,2 +1,7 @@
 console.log(" arguments ")
 console.log(process.argv.some((arg) => arg === '--env'));
+process.argv.forEach((arg) => {
+  if(arg.indexOf('--env')!==-1){
+    console.log("arg" + arg);
+  }
+});
